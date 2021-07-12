622a81453a37c649b522a547a7b46cf204c6a2e2
svasenkov <aanher@gmail.com>
2021-07-12 21:29:51 +0300

    added TestData

---
+++ src/test/java/tests/RegistrationWithTestDataTests.java
@@ -0,0 +1,49 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static tests.TestData.firstName;
+import static tests.TestData.lastName;
+
+public class RegistrationWithTestDataTests {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void successfulRegistrationTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").val(firstName);
+        $("#lastName").val(lastName);
+        $("#userEmail").val("alex@egorov.com");
+        $("[name=gender][value=Other]").parent().click();
+        $("#userNumber").val("1231231231");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2005");
+        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").val("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("./img/1.png");
+        $("#currentAddress").val("Qa guru street 7");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstName + " Egorov"), text("alex@egorov.com"));
+    }
+}
---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class TestData {
+    public static String firstName = "Alex";
+    public static String lastName = "Egorov";
+
+}
